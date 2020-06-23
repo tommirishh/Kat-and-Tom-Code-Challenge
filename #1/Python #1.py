ed5cdb445b5c508b38c1040e9e097cf3df312a7d
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-23 01:59:41 +0100

    Completed with 3 iterations through the list

--- #1/Python #1.py
+++ #1/Python #1.py
@@ -4,14 +4,17 @@ for i in range(4):
     listInp = int(input('enter a number'))
     list.append(listInp)
 
-num = input('Please the sum you wish to find')
+num = int(input('Please the sum you wish to find'))
 print(list)
 
 for i in range(len(list)-1):
-    print(list[0] + list[i+1])
+    if (list[0] + list[i+1]) == num:
+        print(list[0], '+', list[i+1], '=', num)
 
 for i in range(len(list)-2):
-    print(list[1] + list[i+2])
+    if (list[1] + list[i+2]) == num:
+        print(list[1], '+', list[i+2], '=', num)
 
 for i in range(len(list)-3):
-    print(list[2] + list[i+3])
+    if (list[2] + list[i+3]) == num:
+        print(list[2], '+', list[i+3], '=', num)
