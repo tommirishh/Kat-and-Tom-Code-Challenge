7cf58e8ce9705e5a10e5cb2dadff40cc036bf19f
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-23 01:55:54 +0100

    3 loops to add all possible combinations
    used print to see the results easier, will swap for if statments and return values that add up to 'num'. Then I will attemp with one loop.

--- #1/Python #1.py
+++ #1/Python #1.py
@@ -1,12 +1,17 @@
 list = []
 
 for i in range(4):
-    listInp = input('enter a number')
+    listInp = int(input('enter a number'))
     list.append(listInp)
 
 num = input('Please the sum you wish to find')
+print(list)
 
-for i in range(len(list)):
-    temp = list[i]
-    if temp+list[i+1] == num:
-        print(temp, '+', list[i+1])
+for i in range(len(list)-1):
+    print(list[0] + list[i+1])
+
+for i in range(len(list)-2):
+    print(list[1] + list[i+2])
+
+for i in range(len(list)-3):
+    print(list[2] + list[i+3])
