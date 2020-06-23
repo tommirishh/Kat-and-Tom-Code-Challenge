08cb6f059ce3489a7ce929db022ecceb2461181d
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-23 02:29:50 +0100

    Ditched the or syntax and used more if statements and tried out 'try' syntax
    But cutting out the 'or's and using separate 'if's u can tell the user which sum makes the integer they want. Then to keep it all in one loop use the 'try' syntax to allow the code to check the rest of the numbers in the list without crashing because it starts trying to test numbers outside the range of the loop.

--- #1/Python #1.py
+++ #1/Python #1.py
@@ -8,6 +8,16 @@ num = int(input('Please the sum you wish to find'))
 print(list)
 
 for i in range(len(list)-1):
-    if (list[0] + list[i+1]) == num or (list[1]) + list[i+2]) == num or (list[2]):
+    if (list[0] + list[i+1]) == num:
         print(list[0], '+', list[i+1], '=', num)
+    try:
+        if (list[1] + list[i+2]) == num:
+            print(list[1], '+', list[i+2], '=', num)
+    except:
+        print('out of list range')
+    try:
+        if (list[2] + list[i+3]) == num:
+            print(list[2], '+', list[i+3], '=', num)
+    except:
+        print('out of list range')
 
