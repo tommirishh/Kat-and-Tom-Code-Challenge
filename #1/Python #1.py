f0772d3d8de1b2658f76ef10a2f83a9cd5aac760
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-23 01:46:25 +0100

    first attempt at trying to iterate through the list - too ineffcient

--- #1/Python #1.py
+++ #1/Python #1.py
@@ -4,4 +4,9 @@ for i in range(4):
     listInp = input('enter a number')
     list.append(listInp)
 
-print(list)
+num = input('Please the sum you wish to find')
+
+for i in range(len(list)):
+    temp = list[i]
+    if temp+list[i+1] == num:
+        print(temp, '+', list[i+1])
