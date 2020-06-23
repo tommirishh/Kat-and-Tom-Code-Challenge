63c9c802471b4cdf72daf9e8545e487eee4df63c
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-23 02:24:21 +0100

    attempted to use 'or' syntax but wouldnt be able to tell the user which numbers made the sum

--- #1/Python #1.py
+++ #1/Python #1.py
@@ -8,13 +8,6 @@ num = int(input('Please the sum you wish to find'))
 print(list)
 
 for i in range(len(list)-1):
-    if (list[0] + list[i+1]) == num:
+    if (list[0] + list[i+1]) == num or (list[1]) + list[i+2]) == num or (list[2]):
         print(list[0], '+', list[i+1], '=', num)
 
-for i in range(len(list)-2):
-    if (list[1] + list[i+2]) == num:
-        print(list[1], '+', list[i+2], '=', num)
-
-for i in range(len(list)-3):
-    if (list[2] + list[i+3]) == num:
-        print(list[2], '+', list[i+3], '=', num)
