295eaafe67190163be637cc06d8693f9d1417532
kathrynmirish <66796278+kathrynmirish@users.noreply.github.com>
2020-06-24 17:38:51 +0100

    Update Kathryn #1.py
    added answer for bonus question

--- #1/Kathryn #1.py
+++ #1/Kathryn #1.py
@@ -5,64 +5,33 @@
 #For example, given [10, 15, 3, 7] and k of 17, 
 #return true since 10 + 7 is 17
 
-#Bonus - do this in one pass
-
 #NOTES
 #got this running with O(n^2)
 #works for any length of list 
 
-def list_add(x,lis):
+#Bonus - do this in one pass
+#I tested my function and the time taken to run it increases quadratically 
+#I did some research to find a more efficient solution for this bonus 
+#This page explained a more efficient algorithm
+#https://stackoverflow.com/questions/51300360/given-a-list-of-numbers-and-a-number-k-return-whether-any-two-numbers-from-the
+
+def list_add2(x,lis):
     
-    #ends function if x isn't a number
-    if isinstance(x,int)==False:
-        return "input an integer as the first variable"
+    #items need to be in size order
+    lis.sort()
     
-    #ends function if lis isn't a list    
-    if isinstance(lis,list)==False:
-        return "input a list as the second variable"
+    i=0
+    j=len(lis)-1
     
-    #ends function if lis contains anything other than numbers    
-    for i in range(0,len(lis)):
-        if isinstance(lis[i],int)==False:
-            return "input a list of integers as the second variable"
+    # if i meets j all numbers have been tested 
+    while i<j:
+        #if the sum of 2 numbers is too small try the next smallest number
+        if lis[i]+lis[j]<x:
+            i=i+1
+        #if the sum of 2 numbers is too big try the next largest number    
+        elif lis[i]+lis[j]>x:
+            j=j-1
+        #if the sum is equal then return true    
+        else: return True
 
-    
-   # for loop to check each number
-    for i in range (0,len(lis)):
-        
-        #check the difference between x and first list item
-        y=x-lis[0]
-        
-        #remove list item 
-        #this prevents false positive if lis[0]=x/2
-        #also prevents second pass of number 
-        lis.pop(0)
-        
-        #see if the difference is in the list
-        if y in lis:
-            return("True")
-            
-        
-    if len(lis)==0:
-        return("False")
-
-print (list_add(17,[10,11,12,13,14]))
-#result is false
-
-print (list_add(20,[10,12,13,14]))
-#result is false
-
-print (list_add(20,[10,10,12,13]))
-#result is true
-
-print (list_add(5,[1,2,3,4]))
-#result is true
-
-print (list_add("q",[1,2,3,4]))
-# result is "input an integer as the first variable"
-
-print (list_add(2,3))
-# result is "input a list as the second variable"
-
-print (list_add(2,[1,2,3,"q"]))
-# result is "iput a list of integers as the second variable"
+    return False
