62fb9425aa18771752dc3667f18302e091b20be7
tommirishh <56278587+tommirishh@users.noreply.github.com>
2020-06-24 09:16:51 +0100

    Update thomas 2.py
    Added no div function

--- #2/thomas 2.py
+++ #2/thomas 2.py
@@ -16,3 +16,27 @@ def sort_list(list):
 print(sort_list([1,2,3,4,5]))
 
 print(sort_list([3,2,1]))
+
+
+def sort_list_no_division(list):
+
+    productList = []
+
+    for i in range(len(list)):
+        
+        product = 1
+        popped = list.pop()
+        print(list)
+        for i in list:
+            product *= i
+        
+        productList.insert(0,product)
+
+        list.insert(0,popped)
+
+
+    return(productList)
+
+print(sort_list_no_division([1,2,3,4,5]))
+
+print(sort_list_no_division([3,2,1]))
