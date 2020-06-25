7e36de90e14dd69cc4ad9b64e655f6ce9881233c
Kathryn <kathrynmirish@gmail.com>
2020-06-25 18:20:53 +0100

    adding notes to code

--- #4/kathryn4.py
+++ #4/kathryn4.py
@@ -7,15 +7,19 @@ Created on Thu Jun 25 16:41:14 2020
 
 def lowest_int(k):
     
+    #sort from smallest to largest
     k.sort()
     
+    #find positive integers with difference of 2 or more
     for i in range(0,len(k)-1):
         if k[i]>0:
             if k[i+1]-k[i]>1:
                 return k[i]+1
     
+    #if no positive integers have a gap
     if k[-1]>-1:
         return k[-1]+1
+    #if all are negative
     else:
         return 1
             
