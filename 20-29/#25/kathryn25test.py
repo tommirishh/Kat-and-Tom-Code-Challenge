4ddf2980ebeaf86d8c2ae682605bcad361f90502
Kathryn <kathrynmirish@gmail.com>
2020-07-21 19:55:13 +0100

    started working on #25

---
+++ 20-29/#25/kathryn25test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 19:50:15 2020
+
+@author: kmi20042005
+"""
+from kathryn25 import period 
+
+#testing version with just .
+print(period(".ay","ray"))
+print(period(".ay","rays"))
+print(period("pol..","polls"))
